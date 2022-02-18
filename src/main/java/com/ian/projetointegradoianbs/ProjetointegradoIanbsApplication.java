a6a83546f8fd695c5efbdaed01bc2806b6df1d05
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2022-02-18 20:21:39 -0300

    feat: Setup inicial do projeto

---
+++ src/main/java/com/ian/projetointegradoianbs/ProjetointegradoIanbsApplication.java
@@ -0,0 +1,13 @@
+package com.ian.projetointegradoianbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetointegradoIanbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetointegradoIanbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ian/projetointegradoianbs/ProjetointegradoIanbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.projetointegradoianbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetointegradoIanbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
