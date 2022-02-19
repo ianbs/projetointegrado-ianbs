7df0d1c2edd824404eb723f04de1f6e5316f0422
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2022-02-19 11:55:33 -0300

    feat: Criando domain class de Paciene

---
+++ src/main/java/com/ian/projetointegradoianbs/domain/Paciente.java
@@ -0,0 +1,45 @@
+package com.ian.projetointegradoianbs.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+    @Getter
+    @Setter
+    private String nome;
+    @Getter
+    @Setter
+    private String cpf;
+    @Getter
+    @Setter
+    private String rg;
+    @Getter
+    @Setter
+    private String cartaoNacionalSaude;
+    @Getter
+    @Setter
+    private Date dataCadastro;
+    @Getter
+    @Setter
+    private Date dataNascimento;
+
+}
