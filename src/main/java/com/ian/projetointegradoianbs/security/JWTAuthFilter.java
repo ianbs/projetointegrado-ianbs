327f65a1eedfda9e49c6bf4dc4a4b537fe4ee354
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2022-02-21 01:07:04 -0300

    fix: Return do token

--- src/main/java/com/ian/projetointegradoianbs/security/JWTAuthFilter.java
+++ src/main/java/com/ian/projetointegradoianbs/security/JWTAuthFilter.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-// import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ian.projetointegradoianbs.domain.Usuario;
 
 import org.springframework.http.HttpMethod;
@@ -81,7 +80,7 @@ public class JWTAuthFilter extends UsernamePasswordAuthenticationFilter {
 
         response.setContentType("application/json");
         // response.setCharacterEncoding("UTF-8");
-        response.getWriter().print("\"token\": " + token);
+        response.getWriter().write("[{\"token\": " + token + "}]");
         response.getWriter().flush();
     }
 
